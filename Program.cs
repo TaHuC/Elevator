4e1ffbab1f8ec1e198ac140c1d91e1e28348caa5
TaHuC <TaHuC@hotmail.com>
2017-01-24 21:45:47 +0200

    First prefect work

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Elevator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int maxPerson = int.Parse(Console.ReadLine());
+			int capacity = int.Parse(Console.ReadLine());
+
+			int courses = (int)Math.Ceiling((double)maxPerson / capacity);
+
+			Console.WriteLine(courses);
+		}
+	}
+}
